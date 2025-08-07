2473fdb7000d672833a751759f7015018cbfdf9e
navneetshahi14 <navneet.shahi2004@gmail.com>
2025-08-07 18:18:21 +0530

    first commit

---
+++ ChatModels/chatModelsOpenAi.py
@@ -0,0 +1,11 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4",temperature=0.9,max_completion_tokens=200)
+
+result = model.invoke("What is the capital of UttarPradesh")
+
+print(result.content)
